fddc61bcb740b639e3187a93610e00b8a2729da7
Gleb Zaborovskiy <zaborovskij.gleb@gmail.com>
2019-08-24 02:31:05 +0300

    initial commit: generated template libgdx project

---
+++ android/src/com/tallandsmall/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.tallandsmall;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.tallandsmall.KtxGameMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new KtxGameMain(), config);
+	}
+}
---
+++ desktop/src/com/tallandsmall/desktop/DesktopLauncher.kt
@@ -0,0 +1,13 @@
+package com.tallandsmall.desktop
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.tallandsmall.KtxGameMain
+
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = LwjglApplicationConfiguration()
+        LwjglApplication(KtxGameMain(), config)
+    }
+}
